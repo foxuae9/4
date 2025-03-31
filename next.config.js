b424b745cf8d7f8d0cef204fef834417762bf1e9
Temp User <temp@example.com>
2025-03-31 20:07:04 +0400

    Update next.config.js for server deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  poweredByHeader: false,
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || ''
+  },
+  experimental: {
+    outputFileTracingRoot: "/home/ttapp/webapps/ttapp/current"
+  }
 }
 
 module.exports = nextConfig
